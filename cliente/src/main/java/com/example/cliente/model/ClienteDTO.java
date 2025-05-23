699b2de04098e65e706eb8615c4edea23881f538
Vitorwgs <167483937+Vitorwgs@users.noreply.github.com>
2025-05-22 21:12:53 -0300

    Update ClienteDTO.java

--- cliente/src/main/java/com/example/cliente/model/ClienteDTO.java
+++ cliente/src/main/java/com/example/cliente/model/ClienteDTO.java
@@ -4,45 +4,43 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 
 public class ClienteDTO {
-	
-	@NotEmpty(message = "Nome é obrigatório")
-	private String nome;
-	
-	@NotEmpty(message = "Sobrenome é obrigatório")
-	private String sobrenome;
-	
-	@NotEmpty(message = "E-mail é obrigatório")
-	@Email
-	private String email;
 
-	@NotEmpty(message = "Senha é obrigatória")
-	private String senha;
+    @NotEmpty(message = "Nome é obrigatório")
+    private String nome;
 
-	public String getSenha() {
-    	return senha;
-	}
-	public void setSenha(String senha) {
-   	this.senha = senha;
-	}
-	
-	public String getNome() {
-		return nome;
-	}
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-	public String getSobrenome() {
-		return sobrenome;
-	}
-	public void setSobrenome(String sobrenome) {
-		this.sobrenome = sobrenome;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	
-	
+    @NotEmpty(message = "Sobrenome é obrigatório")
+    private String sobrenome;
+
+    @NotEmpty(message = "E-mail é obrigatório")
+    @Email
+    private String email;
+
+    @NotEmpty(message = "Senha é obrigatória")
+    private String senha;
+
+    public String getSenha() {
+        return senha;
+    }
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getSobrenome() {
+        return sobrenome;
+    }
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
